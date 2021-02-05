677b245fa05474bcaf4a85edae24a2311f6892c4
awesomerobot <kris.aubuchon@discourse.org>
2021-02-04 23:30:52 -0500

    bump version

--- lib/discourse_fonts.rb
+++ lib/discourse_fonts.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module DiscourseFonts
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 
   def self.path_for_fonts
     File.expand_path("../../vendor/assets/fonts", __FILE__)
